12ead4a702118df1884fd81b619f7ce2632d9e8a
Kris1992-prog <italian_boy@live.it>
2026-08-04 17:41:42 +0200

    modifiche: risorsa aws instance

--- main.tf
+++ main.tf
@@ -32,7 +32,8 @@ resource "aws_instance" "mio_primo_server" {
   instance_type          = var.instance_type
   key_name               = "MioServerKeyMilano"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-
+  http_tokens = "required"
+  
   # CONFIGURAZIONE AUTOMATICA (USER DATA):
   user_data = <<-EOF
               #!/bin/bash

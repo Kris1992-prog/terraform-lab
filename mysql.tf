fd3be036f2701224a8b04fd51e6da6d09db19ef1
Kris1992-prog <italian_boy@live.it>
2026-08-04 19:33:36 +0200

    fix: risolti errori di sintassi

--- mysql.tf
+++ mysql.tf
@@ -20,18 +20,20 @@ resource "aws_security_group" "db_sg" {
 
 # 2. IL DATABASE RDS MYSQL
 resource "aws_db_instance" "ecommerce_db" {
-  allocated_storage = 20
-  db_name           = "ecommercedb"
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"
-
-  username = "kris_admin"
-  password = var.db_password
+  allocated_storage       = 20
+  db_name                 = "ecommercedb"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  storage_encrypted       = true  #  MODIFICA: Abilita la cifratura dello storage RDS
+  backup_retention_period = 7     # Conservazione dei backup automatici per 7 giornis
+  publicly_accessible     = false # Impedisce l'accesso pubblico diretto al database
+  deletion_protection     = true  # Protezione dalla cancellazione accidentale
+  username                = "kris_admin"
+  password                = var.db_password
 
   parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
-  publicly_accessible  = false
 
   vpc_security_group_ids = [aws_security_group.db_sg.id]
 }
